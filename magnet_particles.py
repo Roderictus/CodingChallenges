ab4acd71df41e6a54f187211ab967c694085a5d0
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-11 20:37:41 +0200

    magnet box

---
+++ magnet_particles.py
@@ -0,0 +1,15 @@
+def doubles(maxk, maxn):
+    # your code
+
+    return 0
+
+
+
+
+def sum_k(maxk, n):
+    sumk = 0
+    for k in range(1,maxk):
+        sumk += 1/(k*(n+1)^2*k)
+        print(sumk)
+
+
