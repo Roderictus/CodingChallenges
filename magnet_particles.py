5d5fd3e364985f9c279eeec2c913157197b9e06e
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-12 00:20:07 +0200

    terminado?

--- magnet_particles.py
+++ magnet_particles.py
@@ -1,36 +1,49 @@
 def doubles(maxk, maxn):
-    def sum_k(maxk, n):
-        sumk = 0
-        for k in range(1, maxk + 1):
-            sumk += 1 / (k * (n + 1) ** (2 * k))
-            print(sumk)
-        return sumk
+    def knesimo(k,n):
+        term = 1 / (k * (n + 1) ** (2 * k))
+        return term
+    sol = 0
 
-    def sum_n(k, maxn):
-        sumn = 0
+    for k in range(1,maxk + 1):
         for n in range(1, maxn + 1):
-            sumn += 1 / (k * (n + 1) ** (2 * k))
-            print(sumn)
-        return sumn
-
-    # your code
-
-    return 0
-
-def sum_k(maxk, n):
-    sumk = 0
-    for k in range(1,maxk +1):
-        sumk += 1/(k*(n+1)**(2*k))
-        print(sumk)
-    return sumk
-
-def sum_n(k, maxn):
-    sumn = 0
-    for n in range(1,maxn +1):
-        sumn += 1/(k*(n+1)**(2*k))
-        print(sumn)
-    return sumn
-
-
-temp = sum_n(2,10)
+            sol = sol + knesimo(k,n)
+    return sol
+temp = doubles(10,10)
 print(temp)
+
+
+#     def sum_k(maxk, n):
+#         sumk = 0
+#         for k in range(1, maxk + 1):
+#             sumk += 1 / (k * (n + 1) ** (2 * k))
+#             print(sumk)
+#         return sumk
+#
+#     def sum_n(k, maxn):
+#         sumn = 0
+#         for n in range(1, maxn + 1):
+#             sumn += 1 / (k * (n + 1) ** (2 * k))
+#             print(sumn)
+#         return sumn
+#
+#     # your code
+#
+#     return 0
+#
+# def sum_k(maxk, n):
+#     sumk = 0
+#     for k in range(1,maxk +1):
+#         sumk += 1/(k*(n+1)**(2*k))
+#         print(sumk)
+#     return sumk
+#
+# def sum_n(k, maxn):
+#     sumn = 0
+#     for n in range(1,maxn +1):
+#         sumn += 1/(k*(n+1)**(2*k))
+#         print(sumn)
+
+#
+#
+# temp = sum_n(2,10)
+# print(temp)
