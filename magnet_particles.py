21aa85ec70d772f154fbb22fc8f0766868f6a06d
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-12 10:33:43 +0200

    terminado

--- magnet_particles.py
+++ magnet_particles.py
@@ -1,5 +1,4 @@
 def doubles(maxk, maxn):
-
     def knesimo(k,n):
         term = 1 / (k * (n + 1) ** (2 * k))
         return term
@@ -7,9 +6,19 @@ def doubles(maxk, maxn):
 
     for k in range(1,maxk + 1):
         for n in range(1, maxn + 1):
-            sol = sol + knesimo(k,n)
+            temp = knesimo(k,n)
+            sol = sol + temp
+            if temp <  0.000000000000000001:
+                break
+
     return sol
-temp = doubles(1000,10000)
+
+# Hacer una lista iterable
+
+
+
+
+temp = doubles(700,700)
 print(temp)
 
 # print(temp)
