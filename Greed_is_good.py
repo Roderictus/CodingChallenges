3411a9a4c13ed9daee8c4deee329025bc4be3867
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-21 12:11:07 +0200

    solución

--- Greed_is_good.py
+++ Greed_is_good.py
@@ -60,3 +60,17 @@ lista = [1, 1, 2, 5, 5]#), 450)
 #lista = [1,1,2,2,3,3]
 temp = score(lista)
 print(temp)
+
+
+def score(dice):
+    sum = 0
+    counter = [0, 0, 0, 0, 0, 0]
+    points = [1000, 200, 300, 400, 500, 600]
+    extra = [100, 0, 0, 0, 50, 0]
+    for die in dice:
+        counter[die - 1] += 1
+
+    for (i, count) in enumerate(counter):
+        sum += (points[i] if count >= 3 else 0) + extra[i] * (count % 3)
+
+    return sum
