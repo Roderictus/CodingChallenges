e7a7e68b0c2482ebc3e70c335ac97585d3742648
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-23 22:18:53 +0200

    necesita una funcion recursiva que guarde la historia de caminos posibles.

--- square_sums.py
+++ square_sums.py
@@ -26,15 +26,24 @@ def squareSumsRow(number):
     usable_squares = [item ** 2 for item in list if item ** 2 <= list[-1] + list[-2]]
     acceptable_pair =aceptable_pairs(list, usable_squares)
     inicial = flatten_and_count(acceptable_pair)
+    print("esto es inicial")
+    print(inicial)
 
     inicial = inicial[0]
     list.remove(inicial)
     sol = []
     sol.append(inicial)
+    acceptableStep = []
+
     for number in list:
         for element in acceptable_pair:
-            if element[0] == sol[-1]:
-                sol.append(element[1])
+            if element == sol[-1]:
+                acceptableStep.append(element)
+                # si hay sólo un camino que funciona podemos agregarlo a la lista de soluciones
+                if len(acceptableStep) == 1:
+                    sol.append(element[1])
+                # si hay más de un camino hay que explorarlos
+                else
                 print(sol)
 
 
@@ -45,6 +54,9 @@ def squareSumsRow(number):
 temp =squareSumsRow(15)
 
 print(temp)
+# todo crear un método para sublistas de profundidad variable
+# todo crear función para borrar pareja ya utilizada
+# todo ordenar los pares viables de acuerdo a los que tienen menor probabilidad de ocurrencia
 # any(x in [2,1,8] for x in [3,4,5,6])
 # Para revisdar si están todos los números
 # number_list = []
@@ -53,3 +65,5 @@ print(temp)
 #         number_list.append(number)
 #
 # print(set(number_list))
+
+#si se usa 8,1,3 quitar 8,1 1,8 1,3, 3,1 de la lista
