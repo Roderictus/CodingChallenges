c484ef141d6f5c280cdb83d88464389d455cfdd4
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-25 21:51:05 +0200

    optimizar limitando opciones a pares válidos

--- square_sums.py
+++ square_sums.py
@@ -1,4 +1,4 @@
-def squareSumsRow(number):
+def square_sums(number):
     def aceptable_pairs(lista, usables):
         acceptable_pair = []
         for item in lista:
@@ -8,52 +8,102 @@ def squareSumsRow(number):
                     acceptable_pair.append([item, numero])
                     #acceptable_pair.append([numero,item])
         return acceptable_pair
+    def backtrack(remaining, current_sequence):
+        if not remaining:
+            return current_sequence
+        last_num = current_sequence[-1]
+        for num in remaining:
+            if (last_num + num) in usable_squares:
+                new_sequence = current_sequence + [num]
+                print(new_sequence)
+                #time.sleep(1)
+                new_remaining = remaining - {num}
+                result = backtrack(new_remaining, new_sequence)
+                if result:
+                    return result
+        return None
 
-    def flatten_and_count(nested_list):
-        flatten = [num for sublist in nested_list for num in sublist]
-        occurrences = {}
-        for num in range(1,number + 1):
-            #print(num)
-            occurrences[num] = flatten.count(num)
-        inicial = []
-        for key,value in occurrences.items():
-            if value == 2:
-                inicial.append(key)
-        print(occurrences)
-        return inicial
+    lista_numeros = [number for number in range(1,number +1)]
+    usable_squares = [item ** 2 for item in lista_numeros if item ** 2 <= lista_numeros[-1] + lista_numeros[-2]]
+    #
+    for start in range(1, number + 1):
+        sequence = backtrack(set(range(1, number + 1)) - {start}, [start])
+        if sequence:
+            return sequence
+    return None
 
-    list = [number for number in range(1,number +1)]
-    usable_squares = [item ** 2 for item in list if item ** 2 <= list[-1] + list[-2]]
-    acceptable_pair =aceptable_pairs(list, usable_squares)
-    inicial = flatten_and_count(acceptable_pair)
-    print("esto es inicial")
-    print(inicial)
-
-    inicial = inicial[0]
-    list.remove(inicial)
-    sol = []
-    sol.append(inicial)
-    acceptableStep = []
-
-    for number in list:
-        for element in acceptable_pair:
-            if element == sol[-1]:
-                acceptableStep.append(element)
-                # si hay sólo un camino que funciona podemos agregarlo a la lista de soluciones
-                if len(acceptableStep) == 1:
-                    sol.append(element[1])
-                # si hay más de un camino hay que explorarlos
-                else
-                print(sol)
-
-
-
-    return acceptable_pair
-
-
-temp =squareSumsRow(15)
+temp = square_sums(5)
 
 print(temp)
+
+    # def flatten_and_count(nested_list):
+    #     flatten = [num for sublist in nested_list for num in sublist]
+    #     occurrences = {}
+    #     for num in range(1,number + 1):
+    #         #print(num)
+    #         occurrences[num] = flatten.count(num)
+    #     inicial = []
+    #     for key,value in occurrences.items():
+    #         if value == 2:
+    #             inicial.append(key)
+    #     print(occurrences)
+    #     return inicial
+
+
+
+    #lista_numeros.remove(inicial)
+    # sol = [[8,1]]
+    # #sol.append(inicial)
+    # acceptableStep = []
+    # print("Esto es lista")
+    # print(lista_numeros)
+    # print("Esto es acceptable pair")
+    # print(acceptable_pair)
+    #
+    # while len(sol) < len(lista_numeros):
+    #     valores_aceptables = []
+    #     for element in acceptable_pair:
+    #         if element[0] == sol[-1][1]:
+    #             print(f"valor aceptable {element}")
+    #             valores_aceptables.append(element)
+    #     print("Estos son valores aceptables")
+    #     print(valores_aceptables)
+    #     sol += 1
+    #
+    #     for valor in valores_aceptables:
+    #         for element in acceptable_pair:
+    #
+    # def recursive:
+
+
+
+
+    # for number in lista_numeros:
+    #     for element in acceptable_pair:
+    #        #print(element)
+    #         if element[0] == sol[-1]:
+    #             acceptableStep.append(element)
+    #             #print("Esto es el primer acceptable step")
+    #             #print(acceptableStep)
+    #             # si hay sólo un camino que funciona podemos agregarlo a la lista de soluciones
+    #             # una vez añadido hay que removerlo de la lista
+    #             acceptable_pair.remove(element)
+    #             acceptable_pair.remove([element[1],element[0]])
+    #             print(acceptable_pair)
+    #
+    #             if len(acceptableStep) == 1:
+    #                 sol.append(element[1])
+    #             # si hay más de un camino hay que explorarlos
+    #             #función para profundidad arbitraria
+    #             else:
+    #                 for thing in acceptableStep:
+    #                     print("esto es thing")
+    #                     print(thing)
+    # print(f"esto es sol : {sol}")
+    #
+    #
+    # return acceptable_pair
+
 # todo crear un método para sublistas de profundidad variable
 # todo crear función para borrar pareja ya utilizada
 # todo ordenar los pares viables de acuerdo a los que tienen menor probabilidad de ocurrencia
